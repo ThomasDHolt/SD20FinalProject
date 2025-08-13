5eca60bd3b96433084f8140b2ea881ced7a3cc10
ThomasDHolt <tholt9111@gmail.com>
2025-08-13 09:22:49 +0100

    Update seed file with dummy data

--- client/seed.sql
+++ client/seed.sql
@@ -50,24 +50,38 @@ CREATE TABLE IF NOT EXISTS workouts (
   created_at      TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE IF NOT EXISTS exercise_type (
+CREATE TABLE IF NOT EXISTS exercise_category (
   id    SERIAL PRIMARY KEY,
   type  VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS exercises (
-  id                  SERIAL PRIMARY KEY,
-  exercise_type_id    INT REFERENCES exercise_type(id),
-  sets                INT NOT NULL,
-  reps                INT NOT NULL,
-  weights             INT NOT NULL,
-  workout_id          INT REFERENCES workouts(id)
+  id                        SERIAL PRIMARY KEY,
+  exercise_category_id      INT REFERENCES exercise_category(id),
+  exercise_type             VARCHAR(255) NOT NULL,
+  sets                      INT NOT NULL,
+  reps                      INT NOT NULL,
+  weights                   INT NOT NULL,
+  workout_id                INT REFERENCES workouts(id)
 );
 
-INSERT INTO exercise_type (type) VALUES
+INSERT INTO exercise_category (type) VALUES
 ('ABS'),
 ('ARMS'),
 ('LEGS'),
 ('BACK'),
 ('CHEST'),
 ('FULL BODY');
+
+INSERT INTO users (name, email, "emailVerified", image) VALUES
+('Janette Smith', 'janetteSmith@gmail.com', '2025-06-12', '');
+
+INSERT INTO workouts (title, user_id, scheduled_for) VALUES
+('Leg Day', 1, '2025-08-13'),
+('Bicep Day', 1, '2025-08-14'),
+('Core Day', 1, '2025-08-15');
+
+INSERT INTO exercises (exercise_category_id, exercise_type, sets, reps, weights, workout_id) VALUES
+(3, 'Squats', 4, 10, 10, 1),
+(2, 'Curls', 2, 20, 0, 2),
+(6, 'Climbers', 4, 10, 0, 3);
